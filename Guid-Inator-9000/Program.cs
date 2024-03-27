e427cf26007e1431405eb6d383ba454fa648008b
Jeremiah Weaver <jbw716@users.noreply.github.com>
2024-03-27 15:55:24 -0500

    Initial commit.

---
+++ Guid-Inator-9000/Program.cs
@@ -0,0 +1,19 @@
+﻿var numberOfProcessorsToUse = Environment.ProcessorCount - 4;
+Console.WriteLine($"Using {numberOfProcessorsToUse} processors:");
+while (true)
+{
+    Guid[] guids = new Guid[numberOfProcessorsToUse];
+    var options = new ParallelOptions { MaxDegreeOfParallelism = numberOfProcessorsToUse };
+    Parallel.For(0, numberOfProcessorsToUse, options, i =>
+    {
+        guids[i] = Guid.NewGuid();
+    });
+    Console.WriteLine(string.Join("\n", guids));
+    if (guids.Distinct().Count() != guids.Length)
+    {
+        Console.WriteLine("Duplicate guids found!");
+        break;
+    }
+    Console.CursorTop = 1;
+    Console.CursorLeft = 0;
+}
